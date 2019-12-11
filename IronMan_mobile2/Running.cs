1909704743b9fe63238ccd5078156bb835d85dc5
aliya ilina <psevdointellektuall@gmail.com>
2019-12-11 12:52:30 +0200

    v1.0.0.14

--- IronMan_mobile2/Running.cs
+++ IronMan_mobile2/Running.cs
@@ -1,21 +1,33 @@
-﻿using Android.Media;
+﻿using System.Net.Mime;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Media;
 using Android.OS;
-using Android.Support.V4.App;
+using Android.Transitions;
 using Android.Views;
 using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
 
 namespace IronMan_mobile2
 {
     public class Running : Fragment
     {
+        private bool backIsClicked;
+        public bool scriptIsRunning = true;
+        private TextView pleasewait;
+        private Button back;
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             View view = inflater.Inflate(Resource.Layout.running, container, false);
-            Button back = view.FindViewById<Button>(Resource.Id.back);
-            
+            pleasewait = view.FindViewById<TextView>(Resource.Id.pleasewait);
+            back = view.FindViewById<Button>(Resource.Id.back);
+
             //when "BACK" is clicked
             back.Click += delegate
             {
+                backIsClicked = true;
                 FragmentManager.PopBackStackImmediate(); //replace the fragment with previous (Scripts)
                 MainActivity.HideTabBar(1); //show tab bar
             };
